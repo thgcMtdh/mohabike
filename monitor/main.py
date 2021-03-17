e8fcfb55d1639343f61b7d195891144db8528baf
thgcMtdh <kyouho315@gmail.com>
2021-03-17 13:54:48 +0900

    立ち上げ時の処理を書いた

--- monitor/main.py
+++ monitor/main.py
@@ -1,36 +1,63 @@
-import time
+import time, json
 import responder
 
-# instead of serial com read text file
-rxbuf = open("rxdata.json", "r").read().replace("\n","")
-print(rxbuf)
+# instead of serial com, read text file
+rxbuf0 = open("rxdata.json", "r").read().replace("\n","")
 
-# background task control flag
-flagRun = True
+# flags and variables
+flagSerial = False  # シリアル通信の実行有無 (Trueのときマイコンからinfo受信)
+flagReset = False   # Falseにするとマイコンをリセット
+rxbuf = ""   # マイコンから受け取った文字列
+speed = 0.0  # 走行速度[km/h](メーターに表示)
+Iac = 0.0    # 主電動機電流[A](メーターに表示)
 
 # web server start
 api = responder.API()
 
 @api.background.task
-def recievedata():
-    while (flagRun):
-        time.sleep(2)
-        print("serial...")
+def recieveinfo():
+    global rxbuf, speed, Iac
+    print("[recieveinfo] start serial communication")
+    while flagSerial:
+        # シリアル通信
+        time.sleep(1)
+        rxbuf = rxbuf0  # 実機から文字を受け取る代わりに、ダミーの文字列を利用
+        # デコード
+        rxdict = json.loads(rxbuf)
+        speed = rxdict['speed']
+        Iac = rxdict['Iac']
+    print("[recieveinfo] end serial communication")
             
 @api.route("/")
 def hello_html(req,resp):
+    global flagSerial, flagReset
+    flagSerial = False  # 再読み込み時はマイコンをリセット
+    flagReset = False
+    time.sleep(1)  # 確実にリセットされるまで時間をおく
     resp.html = api.template('index.html')
-    #recievedata()
 
 @api.route("/info")
-def send_speed(req,resp):
+def get_info(req,resp):
     resp.text = rxbuf
 
-@api.route("/stopserial")
-def stop_serial(req,resp):
-    global flagRun
-    flagRun = False
-    resp.text = f"stop serial"
+@api.route("/command")
+async def post_command(req,resp):
+    global flagSerial, flagReset
+    data = await req.media()
+    # シリアル通信を行うバックグラウンドプロセスの立ち上げ
+    if (flagSerial == False) and (data['serial'] == True):
+        flagSerial = True
+        recieveinfo()
+    # 状態の同期
+    flagSerial = data['serial']
+    flagReset = data['reset']
+    # マイコンのGPIOポートへ転送
+
+@api.route("/reset")
+def reset(req,resp):
+    global flagSerial, flagReset
+    flagSerial = False
+    flagReset = False
 
 if __name__ == '__main__':
     api.run(address='0.0.0.0', port=5042)  # 0.0.0.0にすると外部からアクセスできる
--- monitor/static/main.jsx
+++ monitor/static/main.jsx
@@ -1,27 +1,68 @@
-const lampTextList = {power:'アシスト電源', pedal:'ペダル', assist:'アシスト', regen:'回生', alleb:'全電気ブレーキ', battLB:'バッテリ低電圧', trouble:'三相異常'};
+const lampTextList = {power:'インバータ電源', pedal:'ペダル', assist:'アシスト', regen:'回生', alleb:'全電気ブレーキ', battLB:'バッテリ低電圧', trouble:'三相異常', comfailed:'通信異常'};
+const initialCommand = {reset:true, serial:true};
+
+function judgeCurrentState(info) {
+    if (!info) {
+        return null;
+    } else {
+        // マイコンから受け取ったinfoには、数値情報とpower,pedal,troubleが含まれる
+        var result = info;
+        // 数値情報をもとに各種stateを判断
+        result.assist = (info.Tm > 0.0);
+        result.regen = (info.Tm < 0.0);
+        result.alleb = (info.Tm < 0.0 && info.Vs < 0.0);
+        result.battLB = (info.Vdc < 36.0);
+        return result;
+    }
+}
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: null,
+            comfailed: false,
             info: null,  // インバータの動作状態(マイコン→HTML)
-            command: null  // 指令(HTML→マイコン)
+            command: initialCommand  // 指令(HTML→マイコン)
         }
     }
 
     componentDidMount() {
-        fetch('/info')  // 情報を取得し、stateを更新
-            .then(res => res.json())
-            .then((result) => {
-                this.setState({error: false, info: result});
-            });
+        // 情報を取得し、stateを更新
+        fetch('/info')
+        .then(res => res.json())
+        .then(
+            (result) => {
+                result = judgeCurrentState(result);
+                this.setState({comfailed: false, info: result});
+            },
+            (error) => {
+                console.log(error);
+                this.setState({comfailed: true});
+            }
+        );
+
+        // 読み込みが完了したので、指令を送信
+        fetch('/command', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(this.state.command)
+        })
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({comfailed: false})
+            },
+            (error) => {
+                console.log(error);
+                this.setState({comfailed: true})
+            }
+        )
     }
 
     render() {
         return (
             <div>
-                <IndicatorArea info={this.state.info}/>
+                <IndicatorArea info={this.state.info} comfailed={this.state.comfailed}/>
                 <MonitorArea info={this.state.info}/>
             </div>
         );
@@ -29,18 +70,29 @@ class App extends React.Component {
 }
 
 class IndicatorArea extends React.Component {
-    renderLamp(keyname) {
-        if (!this.props.info) {
-            return <IndicatorLamp keyname={keyname} is_on={true}/>;
+    renderLamp(keyname, color) {
+        if (!this.props.info) {  // infoを受け取っていない場合、すべてのランプを点灯
+            var is_on = true;
         } else {
-            return <IndicatorLamp keyname={keyname} is_on={this.props.info[keyname]}/>;
+            if (keyname == 'comfailed') {
+                var is_on = this.props.comfailed;
+            } else {
+                var is_on = this.props.info[keyname];
+            }
         }
+        return <IndicatorLamp keyname={keyname} is_on={is_on} color={color}/>;
     }
     render() {
         return (
-            <div class="indicatorarea">
-                {this.renderLamp('power')}
-                {this.renderLamp('pedal')}
+            <div className="indicatorarea">
+                {this.renderLamp('power', 'indicator_green')}
+                {this.renderLamp('pedal', 'indicator_orange')}
+                {this.renderLamp('assist', 'indicator_orange')}
+                {this.renderLamp('regen', 'indicator_green')}
+                {this.renderLamp('alleb', 'indicator_orange')}
+                {this.renderLamp('battLB', 'indicator_orange')}
+                {this.renderLamp('trouble', 'indicator_red')}
+                {this.renderLamp('comfailed', 'indicator_red')}
             </div>
         )
     }
@@ -48,7 +100,7 @@ class IndicatorArea extends React.Component {
 class IndicatorLamp extends React.Component {
     render() {
         return (
-            <div class={"indicatorlamp indicator_"+this.props.is_on}>
+            <div className={"indicatorlamp lamp_"+this.props.is_on+" "+this.props.color}>
                 {lampTextList[this.props.keyname]}
             </div>
         );
